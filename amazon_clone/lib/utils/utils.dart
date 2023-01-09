36a54e9df16b7e901e3ce36a43b841b184966c58
KarthiDreamer <karthikeyan.jayhind@gmail.com>
2023-01-09 15:34:43 +0530

    error fix

--- amazon_clone/lib/utils/utils.dart
+++ amazon_clone/lib/utils/utils.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Utils{
   Size getScreenSize() {
-    return MediaQueryData.fromWindow(WidgetsBinding.instance!.window).size;
+    return MediaQueryData.fromWindow(WidgetsBinding.instance.window).size;
     
   }
   showSnackBar({required BuildContext context , required String content }){
